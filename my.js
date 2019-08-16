eaa6c2934682647ac3a7eabdaa7aef8a01930622
KazuyaYumita <kazuyaxjp@yahoo.co.jp>
2019-08-16 15:02:12 +0900

    touch able

--- my.js
+++ my.js
@@ -3,16 +3,20 @@
 var Engine = Matter.Engine;
 var World = Matter.World;
 var Bodies = Matter.Bodies;
+var MouseConstraint = Matter.MouseConstraint;
 
 // Matter.js エンジン作成
 var engine = Engine.create(document.body);
 
+var mouseConstraint = MouseConstraint.create(engine);
+World.add(engine.world, mouseConstraint);
+
 // 二つの箱(四角)と地面を作る
-var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxA = Bodies.rectangle(0, 0, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
 
 // isStatic:静的(完全固定)
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var ground = Bodies.rectangle(40, 300, 600, 60, { isStatic: true });
 
 // 二つの箱(四角)と地面を追加
 World.add(engine.world, [boxA, boxB, ground]);
