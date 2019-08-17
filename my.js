6cf109a1d79f1a6b1d81c8d2867981dd4d9ffec3
KazuyaYumita <kazuyaxjp@yahoo.co.jp>
2019-08-17 16:19:55 +0900

    click and create object

--- my.js
+++ my.js
@@ -1,27 +1,66 @@
-(function() {
+//(function() {
 
 var Engine = Matter.Engine;
 var World = Matter.World;
 var Bodies = Matter.Bodies;
+var Render = Matter.Render;
 var MouseConstraint = Matter.MouseConstraint;
 
+var canvas = document.getElementById('canvas')
 // Matter.js エンジン作成
-var engine = Engine.create(document.body);
+var engine = Engine.create();
 
 var mouseConstraint = MouseConstraint.create(engine);
-World.add(engine.world, mouseConstraint);
+
+var render = Render.create({
+    canvas: canvas,
+    engine: engine,
+    options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        background: '#FFF',
+        wireframes: false,
+        showAngleIndicator: false
+    }
+});
+
 
 // 二つの箱(四角)と地面を作る
-var boxA = Bodies.rectangle(0, 0, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+
+var boxA = Bodies.rectangle(450,300,80,80,{
+    render:{
+        fillstroke:'white',
+        strokeStyle: 'blue',
+        lineWidth: 30
+    }
+    
+});
+    
+
+var boxB = Bodies.rectangle(450, 50, 80, 80,{
+});
 
 // isStatic:静的(完全固定)
 var ground = Bodies.rectangle(40, 300, 600, 60, { isStatic: true });
 
 // 二つの箱(四角)と地面を追加
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [boxA,boxB, ground,mouseConstraint]);
 
 // Matter.js エンジン起動
 Engine.run(engine);
+Render.run(render);
 
-})();
+canvas.addEventListener('click',function(){
+    var box = Bodies.rectangle(200, 0, 20, 20,{
+        render:{
+            fillstroke:'red',
+            lineWidth:20
+        }
+    });
+    World.add(engine.world,[box])
+});
+
+//})();
+
+
+
