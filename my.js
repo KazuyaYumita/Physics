8b4f81f660d284d00d1ddd89ee1ed596fab36e6f
KazuyaYumita <kazuyaxjp@yahoo.co.jp>
2019-08-17 20:58:09 +0900

    add collision sound

--- my.js
+++ my.js
@@ -3,6 +3,7 @@ var World = Matter.World;
 var Bodies = Matter.Bodies;
 var Render = Matter.Render;
 var MouseConstraint = Matter.MouseConstraint;
+var Event = Matter.Event;
 
 var canvas = document.getElementById('canvas')
 // Matter.js エンジン作成
@@ -26,12 +27,15 @@ var render = Render.create({
 // 二つの箱(四角)と地面を作る
 
 var boxA = Bodies.rectangle(450,300,80,80,{
+    density:1,
+    friction: 1,
+    restitution: 0.01,
+    frictionAir: 0.1,
     render:{
         fillstroke:'white',
         strokeStyle: 'blue',
         lineWidth: 30
     }
-    
 });
     
 
@@ -40,6 +44,19 @@ var boxB = Bodies.rectangle(450, 50, 80, 80,{
 
 // isStatic:静的(完全固定)
 var ground = Bodies.rectangle(40, 300, 600, 60, { isStatic: true });
+
+var start = new Date();
+var end = new Date();
+
+Matter.Events.on(engine, "collisionStart ", function(){
+    end = new Date();
+    var ms = end.getTime() - start.getTime();
+
+    if(ms >= 100)collisionSound();
+
+    console.log(ms)
+    start = new Date();
+})
 
 // 二つの箱(四角)と地面を追加
 World.add(engine.world, [boxA,boxB, ground,mouseConstraint]);
@@ -59,16 +76,22 @@ canvas.addEventListener(EVENTNAME_TOUCHEND,function(){
         }
     });
     World.add(engine.world,[box]);
-    tone();
+    clickSound();
+    start = new Date();
 });
 
-function tone(){
 
+
+
+function clickSound(){
     var synth = new Tone.Synth().toMaster();
     synth.triggerAttackRelease('A3', '10n');
-
 }
 
+function collisionSound(){
+    var synth = new Tone.MembraneSynth().toMaster();
+    synth.triggerAttackRelease('C3', '10n');
+}
 
 
 
